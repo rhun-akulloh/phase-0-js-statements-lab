67d21033f0d6d99bcd1045894b1027dcac5e05e7
rhun-akulloh <rhunakulloh@gmail.com>
2026-03-23 00:15:52 +0300

    updated gardenManagement.js file

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,24 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+
+//Check temperature
+if (temperature > 80) {
+  console.log("Watering on");
+} else {
+  console.log("Watering off");
+}
+
+//Check time of day
+if (timeOfDay === "evening" || timeOfDay === "night") {
+  console.log("Lights on");
+} else {
+  console.log("Lights off");
+}
+
+//Increment soil moisture
+while (soilMoisture < 40) {
+  soilMoisture += 5;
+  console.log(soilMoisture);
+}
